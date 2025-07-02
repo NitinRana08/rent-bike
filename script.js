efd240ad79ab28bd558d766d44dee9db7d072fd2
NitinRana08 <nitinr8229@gmail.com>
2025-07-03 01:36:34 +0530

    .
    .

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function bookBike(bikeName){
-    localSorage.setItem('selectedBike',bikeName)
+    localSorage.setItem('selectedBike',bikeName) //store in locally
 
     window.location.href = "booking.html" // lets go to booking page bb
+}
+
+function validdatePhone(){
+    const phone = document.getElementById('phone').value
+    if(phone.length !== 10){
+        document.getElementById('error').style.display = 'block'
+        return false; //cant submit bb
+
+    }else
+    return true
 }
